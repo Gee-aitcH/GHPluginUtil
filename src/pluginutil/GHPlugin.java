6a115958230d48625fafd69a4e8199c03f06caa2
(G_H) <ThisGHIsNotThatGH@users.noreply.github.com>
2020-12-30 21:38:48 +0800

    Comments and Comments
    In case I forgot.

--- src/pluginutil/GHPlugin.java
+++ src/pluginutil/GHPlugin.java
@@ -13,6 +13,7 @@ import static pluginutil.PluginUtil.*;
 
 public class GHPlugin extends Plugin {
 
+    // Modify PLUGIN, CMD, DESC and you will be fine.
     protected String
             PLUGIN = "DiscordBot",
             CMD = "killall",
@@ -34,12 +35,11 @@ public class GHPlugin extends Plugin {
         }
     }
 
-    //register commands that run on the server
+    //register commands... no need to override these if you will redirect all commands to commandHandler and use my format.
     @Override
     public void registerServerCommands(CommandHandler handler) {
         handler.register(CMD, "[arg(s)...]", DESC, args -> commandHandler(args[0].split(" "), null, true));
     }
-
     @Override
     public void registerClientCommands(CommandHandler handler) {
         handler.<Player>register(CMD, "[arg(s)...]", DESC, (args, player) -> commandHandler(args[0].split(" "), player, false));
@@ -48,38 +48,43 @@ public class GHPlugin extends Plugin {
     protected void commandHandler(String[] arg, Player player, boolean s){
     }
 
-    protected void send(String color, String str){
-        send(color, str, null);
+    // Send message to all players
+    protected void send(String color, String msg){
+        send(color, msg, null);
     }
-    protected void send(String color, String str, Player player) {
+    // Send message to certain player
+    protected void send(String color, String msg, Player player) {
         if (player == null)
-            Call.sendMessage(plgn + PLUGIN + ": " + color + str);
+            Call.sendMessage(plgn + PLUGIN + ": " + color + msg);
         else
-            player.sendMessage(plgn + PLUGIN + ": " + color + str);
+            player.sendMessage(plgn + PLUGIN + ": " + color + msg);
     }
 
-    protected void log(int mode, String str){
+    // Send message to console, 0: info, 1: warn
+    protected void log(int mode, String msg){
         switch (mode){
-            case 0 -> Log.info(PLUGIN + ": " + str);
-            case 1 -> Log.warn(PLUGIN + ": " + str);
+            case 0 -> Log.info(PLUGIN + ": " + msg);
+            case 1 -> Log.warn(PLUGIN + ": " + msg);
         }
     }
 
     // o Stands for output. Bad name, I know, whatever.
+    // Send message to all players, certain player or console.
     protected void o(int mode, String color, String msg, Player player, boolean server){
         if (server)
             log(mode, msg);
         else
             send(color, msg, player);
     }
-    // Util Stuff
 
+    // Return '/cmd' or 'cmd', depended by whether if it is used to print in chat or console.
     protected String cmd(boolean prefix){
         return (prefix ? "/" : "") + CMD;
     }
 
+    // Write File
     protected void write() {
-        try{
+        /*try{
             FileWriter writer = new FileWriter(CONFIG_DIR);
             writer.write("Version: " + 1.0 + "\n");
             writer.write("Mode: " + mode + "\n");
@@ -87,11 +92,12 @@ public class GHPlugin extends Plugin {
             log(0, "Configs Saved");
         }catch (Exception ee) {
             ee.printStackTrace();
-        }
+        }*/
     }
 
+    // Read file
     protected void read() {
-        try{
+        /*try{
             File file = new File(CONFIG_DIR);
             boolean hasSave = !file.createNewFile();
             Scanner reader = new Scanner(file);
@@ -116,13 +122,19 @@ public class GHPlugin extends Plugin {
             }
 
             // Default values
-            mode = true;
+            defConfig();
 
             write();
 
             log(0, "Config File Created in Directory \n[" + file.getAbsolutePath() + "].");
         }catch (Exception ee) {
+            defConfig();
             ee.printStackTrace();
-        }
+        }*/
+    }
+
+    // Default configs here
+    protected void defConfig(){
+        //mode = true;
     }
 }
